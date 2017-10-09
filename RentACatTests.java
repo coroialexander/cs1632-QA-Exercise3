91d960f79de39ff87ffe1e9d7991bdefbb8fe16d
BDANG <brindang776@aol.com>
2017-10-08 20:27:13 -0400

    revised and added new tests that use doubled/stubbed cats

--- RentACatTests.java
+++ RentACatTests.java
@@ -15,36 +15,74 @@ public class RentACatTests{
     }
 
 
+    // Test to make sure that returnCat() returns true
+    // for a rented cat
+    // use a doubled cat that is already rented
+    @Test
+    public void returnRentedCat(){
+        //double a cat
+        Cat mockCat = Mockito.mock(Cat.class);
+        Mockito.when(mockCat.getRented()).thenReturn(true);
+
+        assertTrue(rac.returnCat(mockCat));
+    }
+
+    // Test to make sure that returnCat() returns false
+    // for an available cat
+    // use a doubled cat that is not rented
+    @Test
+    public void returnAvailableCat(){
+        //double a cat
+        Cat mockCat = Mockito.mock(Cat.class);
+        Mockito.when(mockCat.getRented()).thenReturn(false);
+
+        assertFalse(rac.returnCat(mockCat));
+    }
+
     // Test to make sure that rentCat() method returns false
     // for unavailable cats
-    // uses a mock cat that is already rented
+    // uses a doubled cat that is already rented
     @Test
     public void rentUnavailableCat(){
-        //mock a cat
+        //double a cat
         Cat mockCat = Mockito.mock(Cat.class);
+        Mockito.when(mockCat.getRented()).thenReturn(true);
 
-        //mockCat.when(mockCat.getRented()).thenReturn(false);
+        assertFalse(rac.rentCat(mockCat));
+    }
+
+    // Test to make sure that rentCat() method returns true
+    // for available cats
+    // uses a doubled cat that is not rented
+    @Test
+    public void rentAvailableCat(){
+        //double a cat
+        Cat mockCat = Mockito.mock(Cat.class);
+        Mockito.when(mockCat.getRented()).thenReturn(false);
+
         assertTrue(rac.rentCat(mockCat));
     }
 
 
-
-
-
+    // Test to make sure that a cat in ._cats returns true
+    // for a cat with an ID of the passed ID in .catExists()
     @Test
     public void testCatExists(){
-        assertTrue(rac.catExists(2, rac._cats));
+        Cat mockCat = Mockito.mock(Cat.class);
+        Mockito.when(mockCat.getId()).thenReturn(10);
+
+        rac._cats.add(mockCat);
+        assertTrue(rac.catExists(10, rac._cats));
     }
 
+    // Test to make sure that a cat in ._cats returns false
+    // for no cats with an ID of the passed ID in .catsExists()
     @Test
     public void testCatNotExists(){
+        Cat mockCat = Mockito.mock(Cat.class);
+        Mockito.when(mockCat.getId()).thenReturn(99);
+
+        rac._cats.add(mockCat);
         assertFalse(rac.catExists(100, rac._cats));
-    }
-
-
-
-    @Test
-    public void tautology(){
-        assertTrue(true);
     }
 }
